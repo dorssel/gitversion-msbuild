0303e6863bf06da5420983a6b8baae22b455a021
Frans van Dorsselaer <17404029+dorssel@users.noreply.github.com>
2024-11-20 21:47:50 +0100

    Add unit tests

---
+++ UnitTests/MSTestSettings.cs
@@ -0,0 +1,6 @@
+﻿// SPDX-FileCopyrightText: 2024 Frans van Dorsselaer
+//
+// SPDX-License-Identifier: MIT
+
+[assembly: Parallelize(Scope = ExecutionScope.MethodLevel)]
+[assembly: DiscoverInternals]
---
+++ UnitTests/Test1.cs
@@ -0,0 +1,14 @@
+﻿// SPDX-FileCopyrightText: 2024 Frans van Dorsselaer
+//
+// SPDX-License-Identifier: MIT
+
+namespace UnitTests;
+
+[TestClass]
+sealed class Test1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
