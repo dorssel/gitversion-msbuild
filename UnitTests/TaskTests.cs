80d2427fca6617ec594bc0daaf8933af7b87fcbd
Frans van Dorsselaer <17404029+dorssel@users.noreply.github.com>
2024-11-21 21:00:53 +0100

    Add unit tests

---
+++ UnitTests/TaskTests.cs
@@ -0,0 +1,39 @@
+﻿// SPDX-FileCopyrightText: 2024 Frans van Dorsselaer
+//
+// SPDX-License-Identifier: MIT
+
+using System.Reflection;
+
+namespace UnitTests;
+
+[TestClass]
+sealed class TaskTests
+{
+    public required TestContext TestContext { get; set; }
+
+    static string GetVersionFile()
+    {
+        for (var directory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            directory is not null; directory = Path.GetDirectoryName(directory))
+        {
+            var file = Path.Combine(directory, "obj", "gitversion.json");
+            if (File.Exists(file))
+            {
+                return file;
+            }
+        }
+        throw new FileNotFoundException("gitversion.json");
+    }
+
+    [TestMethod]
+    public void GenerateGitVersionInformation_WithIntermediateOutputPath()
+    {
+        var task = new Dorssel.GitVersion.MsBuild.GenerateGitVersionInformation
+        {
+            VersionFile = GetVersionFile(),
+            IntermediateOutputPath = TestContext.TestRunDirectory!
+        };
+        var result = task.Execute();
+        Assert.IsTrue(result);
+    }
+}
